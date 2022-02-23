42317f887b6a068468d29f588d72f2db2d0b56a8
devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-02-23 20:29:37 +0200

    command register

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import nekokai_commandregister from "./libs/command-register";
 import nekokai_client from "./libs/client";
 
 // Functions
-nekokai_client()
+nekokai_client();
+nekokai_commandregister();
--- src/libs/command-register.ts
+++ src/libs/command-register.ts
@@ -1,2 +1,7 @@
+import { Registry } from '@skyra/http-framework';
 export default async function nekokai_commandregister() {
+    const registry = new Registry({ token: process.env.DISCORD_TOKEN })
+    await registry.load();
+    await registry.registerGlobalCommands();
+    await registry.registerGuildRestrictedCommands();
 }
