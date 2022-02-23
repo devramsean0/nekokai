28ac7f90c953636d538ee5c3cd944ef650e6b20e
devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-02-23 20:25:40 +0200

    Client function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 // imports
 import nekokai_commandregister from "./libs/command-register";
 import nekokai_client from "./libs/client";
+
+// Functions
+nekokai_client()
--- src/libs/client.ts
+++ src/libs/client.ts
@@ -1,3 +1,6 @@
+import { Client } from '@skyra/http-framework';
 export default async function nekokai_client() {
-    
+const client = new Client({ discordPublicKey: process.env.DISCORD_PUBLIC_KEY});
+await client.load();
+await client.listen({ port: process.env.WEBSERVER_PORT });
 }
